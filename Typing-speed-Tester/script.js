df12034dc21baf3e5f061c669cad64e2a231ed19
krishnakeshariya1 <keshariyakrishna8@gmail.com>
2025-11-14 15:55:12 +0530

    making logics of tester

--- Typing-speed-Tester/script.js
+++ Typing-speed-Tester/script.js
@@ -1,3 +1,35 @@
+// -----Typing-Tester-----
+
+// ---Select Element---
 const randomPara = document.querySelector("#randomPara");
 const startBtn = document.querySelector("#startBtn");
 const resultPara = document.querySelector("#speedCalculate");
+const textArea = document.querySelector("#textArea");
+
+// add eventListener to button
+
+function renderSpan(line){
+    randomPara.innerHTML = "";
+    const spanArray = [];
+    for(let i = 0 ; i<line.length; i++){
+        const span = document.createElement("span");
+        span.textContent = line[i];
+        spanArray.push(span);
+        randomPara.appendChild(span)
+    }
+    return spanArray;
+}
+let spans = renderSpan("hello world");
+let index =0;
+textArea.addEventListener("input", ()=>{
+    const typed = textArea.value;
+
+    if(index >= spans.length) return;
+
+    const correctType = spans[index].textContent;
+    const typedText = typed[typed.length-1];
+    spans[index].classList.add(typedText === correctType ? "correct" : "wrong");
+
+    index ++;
+});
+
