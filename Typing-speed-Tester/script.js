9e6bdf4febae749c15f63ae6b9c61aa16e8c2e98
krishnakeshariya1 <keshariyakrishna8@gmail.com>
2025-11-15 13:22:34 +0530

    working on project

--- Typing-speed-Tester/script.js
+++ Typing-speed-Tester/script.js
@@ -23,11 +23,13 @@ let spans = renderSpan("hello world");
 let index =0;
 textArea.addEventListener("input", ()=>{
     const typed = textArea.value;
+    console.log(typed)
 
     if(index >= spans.length) return;
 
     const correctType = spans[index].textContent;
     const typedText = typed[typed.length-1];
+    console.log(typedText)
     spans[index].classList.add(typedText === correctType ? "correct" : "wrong");
 
     index ++;
