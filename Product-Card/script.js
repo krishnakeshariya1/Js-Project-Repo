238d52570498c3fc5440dfee68a70d8a353c1d3f
krishnakeshariya1 <keshariyakrishna8@gmail.com>
2025-11-26 14:25:11 +0530

    rendercard function

--- Product-Card/script.js
+++ Product-Card/script.js
@@ -1,11 +1,48 @@
 // ----------------- product Card ---------------- //
+// -----------------  Elements  ------------------//
+const exploreSection = document.getElementById("explore-Section");
+const gridContainer = document.querySelector(".grid-Container");
 
 // --------------------  API  ------------------ //
 const url = "https://dummyjson.com/products/category/mens-shoes";
 
-async function fetchShoes() {
-    
-}
+function renderCard(){
 
-https://img.freepik.com/premium-psd/running-shoes-sneakers-transparent-background_84443-1639.jpg
-https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTM6xXnJVaOl7YR4TdebXSltGNGNkkyCY9g3Q&s
+const card = document.createElement("div");
+card.classList.add("card");
+
+const cardImg = document.createElement("div");
+cardImg.classList.add("card-img");
+
+const img = document.createElement("img");
+img.src = "";
+img.alt = "";
+
+cardImg.appendChild(img);
+
+const cardText = document.createElement("div");
+cardText.classList.add("card-Text");
+
+const title = document.createElement("h3");
+title.textContent = "Title";
+
+const price = document.createElement("p");
+price.textContent = "$12";
+
+const category = document.createElement("p");
+category.textContent = "Category: men shoes";
+
+const rating = document.createElement("p");
+rating.textContent = "Rating: 5 star";
+
+const stock = document.createElement("p");
+stock.textContent = "Stock: 8";
+
+cardText.append(title, price, category, rating, stock);
+
+card.append(cardImg, cardText);
+
+gridContainer.appendChild(card);
+
+}
+renderCard();
